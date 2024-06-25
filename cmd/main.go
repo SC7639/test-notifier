c81f5230890725880c2d3062f7c6ee29a733f68b
Scott Crossan <scrott@gmail.com>
2024-06-25 12:44:03 +0100

    Initialize project

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/SC7639/test-notifier
+
+go 1.16
+
+require (
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/joshdk/go-junit v1.0.0 // indirect
+)
